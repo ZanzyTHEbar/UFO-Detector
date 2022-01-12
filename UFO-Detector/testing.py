962d84700b1c3caf59cadcd5a57a9c74c871baf3
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-01-12 01:20:08 +0100

    edit testing.py to be functional - full model and is now functional.

--- UFO-Detector/testing.py
+++ UFO-Detector/testing.py
@@ -5,9 +5,8 @@
 import base64
 import requests
 
-
 # Save string of image file path below
-string_of_image_file_path = "DataSet\images\Birds\B0000000.jpg"
+string_of_image_file_path = "C:/Users/zacar/Documents/GitHub/UFO-Detector/DataSet/images/Birds/B0000000.jpg"
 img_filepath = string_of_image_file_path
 
 # Create base64 encoded string
