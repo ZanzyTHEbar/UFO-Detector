92ca702375b6ab1c072a976f597bc45226ac3693
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-01-19 01:36:20 +0100

    fixed video-capture function now fully functional

--- UFO-Detector/testing.py
+++ UFO-Detector/testing.py
@@ -6,7 +6,9 @@ import cv2
 
 
 def get_prediction_from_image_url(argv):
-    image_to_predict = input("Enter the folder path and image name of them image to predict: ") # ex: "/airplanes/00000001.jpg"
+    # ex: "/airplanes/00000001.jpg"
+    image_to_predict = input(
+        "Enter the folder path and image name of them image to predict: ")
     input_values = image_to_predict
     # Save string of image file path below
     string_of_image_file_path = "C:/Users/zacar/Documents/GitHub/UFO-Detector/DataSet/images/" + input_values
@@ -28,22 +30,25 @@ def get_prediction_from_image_url(argv):
 
     # Print the top predicted label and its confidence
     print("predicted label:\t{}\nconfidence:\t\t{}"
-        .format(top_prediction["label"], top_prediction["confidence"]))
-    
-    
+          .format(top_prediction["label"], top_prediction["confidence"]))
+
+
 def get_prediction_video(argv):
-    
+
     cap = cv2.VideoCapture(0)
-    
+
     while (True):
         # Capture frame-by-frame
-        ret, frame = base64.b64encode(cap.read()).decode("utf-8")
+        ret, frame = cap.read()
+        # encode as base64 jpeg
+        result, jpg = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
+        encodedimage = base64.b64encode(jpg)
 
         # Get response from POST request
         # Update the URL as needed
         response = requests.post(
             url="http://localhost:5000/predict",
-            json={"image": frame},
+            json={"image": encodedimage.decode("utf-8")},
         )
 
         data = response.json()
@@ -51,10 +56,10 @@ def get_prediction_video(argv):
 
         # Print the top predicted label and its confidence
         print("predicted label:\t{}\nconfidence:\t\t{}"
-            .format(top_prediction["label"], top_prediction["confidence"]))
-        
+              .format(top_prediction["label"], top_prediction["confidence"]))
+
         # Display the resulting frame
-        cv2.imshow('frame',frame)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -62,7 +67,7 @@ def get_prediction_video(argv):
     cap.release()
     cv2.destroyAllWindows()
 
-    
+
 if __name__ == "__main__":
-    sys.exit(get_prediction_from_image_url(sys.argv))
-    # sys.exit(get_prediction_video(sys.argv))
+    # sys.exit(get_prediction_from_image_url(sys.argv))
+    sys.exit(get_prediction_video(sys.argv))
